560261e40c132241271b5dfe74daf562219aa0c5
klcb19 <kerencaldera16@gmail.com>
2025-10-28 13:09:41 -0500

    avanzado

--- 02-sintaxis-react/src/components/NavBar.jsx
+++ 02-sintaxis-react/src/components/NavBar.jsx
@@ -4,10 +4,9 @@ import "../stylesheets/NavBar.css"
 
 export const NavBar = () => {
     return (
-        <div>
+        <div className="barra">
             <div>
-                <img src="" alt="" />
-                <h2>Extensions</h2>
+                <h2>🛑Extensions</h2>
             </div>
             <button className= "btn-mode">☀️</button>
         </div>
