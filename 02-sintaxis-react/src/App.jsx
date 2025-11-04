f84eedb1b65377a402b4d0ec980844249ff185b8
klcb19 <kerencaldera16@gmail.com>
2025-11-04 13:23:12 -0500

    cambios

--- 02-sintaxis-react/src/App.jsx
+++ 02-sintaxis-react/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css"
 import { NavBar } from "./components/NavBar";
+import { ExtensionCard } from "./components/ExtensionCard";
 
  
 export const App = () => {
   return (
     <>
     <NavBar />
+    <ExtensionCard/>
     </>
   )
 }
