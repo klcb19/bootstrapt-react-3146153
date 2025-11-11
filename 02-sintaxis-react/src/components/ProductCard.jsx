320eaa95635db8b455922957cc8b77a0c3c039bb
klcb19 <kerencaldera16@gmail.com>
2025-11-11 13:18:06 -0500

    seccioon

---
+++ 02-sintaxis-react/src/components/ProductCard.jsx
@@ -0,0 +1,13 @@
+export function ProductCard(params) {
+    return (
+        <div className="card">
+            <div className="bg-secondary rounded-circle m-auto mt-1" style={{ width: "80px", height: "80px" }}></div>
+            <div className="card-body text-center">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                <a href="#" className="btn btn-secondary">View details</a>
+            </div>
+        </div>
+    )
+
+}
