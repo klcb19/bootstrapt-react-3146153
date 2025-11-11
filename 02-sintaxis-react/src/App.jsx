9b50a10a1d51972c9703c6537a222f0e33c10911
klcb19 <kerencaldera16@gmail.com>
2025-11-11 13:17:33 -0500

    seccion card terminado

--- 02-sintaxis-react/src/App.jsx
+++ 02-sintaxis-react/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css"
 import { NavBar } from "./components/NavBar";
 import { ExtensionCard } from "./components/ExtensionCard";
 import { LoginForm } from "./components/LoginForm";
+import { ProductCard } from "./components/ProductCard";
+
 
 
 export const App = () => {
@@ -60,6 +62,21 @@ export const App = () => {
       </div>
       <h1>React + Bootstrap</h1>
       <LoginForm/>
+
+      <div className="container">
+        <div className="row">
+        <div className="col-12 col-md-6 col-lg-4">
+          <ProductCard/>
+        </div>
+        <div className="col-12 col-md-4 col-lg-4 mt-4 mt-md-0">
+          <ProductCard/>
+        </div>
+        <div className="col-12 col-md-12 col-lg-4 mt-4 mt-lg-0">
+          <ProductCard/>
+          </div>
+        </div>
+      </div>
+      
 
     </>
   )
